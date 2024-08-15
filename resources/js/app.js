d4386f10541eb9645b97abe70ba88a1aeac35bc5
AJ <yogalajay@gmail.com>
2024-08-15 21:19:13 +0545

    installed livewire and alpine-ajax

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import ajax from '@imacrayon/alpine-ajax'
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+
+Alpine.plugin(ajax)
+Livewire.start()
