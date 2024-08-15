b8ff495eac4e1b4c11fd07a8c26823ce8f27b3f6
AJ <yogalajay@gmail.com>
2024-08-15 21:08:28 +0545

    Todo add delete

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Models\Todo;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    sleep(2);
+    $todos = Todo::all();
+    return view('welcome', [
+        'todos' => $todos
+    ]);
+});
+
+
+Route::post('/todo', function(Request $request){
+
+    $todo_title = $request->input("title");
+
+    Todo::create([
+        'title' => $todo_title
+    ]);
+
+    return redirect()->back();
+});
+
+Route::delete('/todo/{id}', function(Request $request, $id){
+    $todo = Todo::find($id);
+    $todo->delete();
+
+    return redirect()->back();
 });
